9f04eecbd809e1bcba2c899cc5390bfa1b5c0591
ahmeterdincer <ahmeterdincer889@gmail.com>
2025-08-17 18:33:12 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+const temp = document.getElementById("temperature");
+const iconDiv = document.getElementById("icon");
+const cityInput = document.getElementById("cityInput");
+const weatherForm = document.getElementById("weatherForm");
+const details = document.getElementById("details");
+const description = document.getElementById("description");
+
+
+async function getWeather() {// şehirleri ayarlama
+    try {
+        const apiKey = "08202a6bbdcfcf7dd253f9de5fcf166c";
+        const city = cityInput.value.trim();
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data);
+        iconDiv.innerHTML = `<img src=http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png>`;
+        temp.textContent = `${data.main.temp} °C`;
+        details.innerHTML = `<p>Hissedilen :<span> ${data.main.feels_like} °C</span></p> <p>Nem Oranı : <span>${data.main.humidity} %</span></p> 
+        <p>Rüzgar : <span>${data.wind.speed} m/s</span></p>`;
+        description.textContent = '';
+    }
+    catch (error) {
+        console.error("Hata:", error);
+        temp.textContent = '';
+        iconDiv.innerHTML = '';
+        details.innerHTML = '';
+        description.textContent = 'Şehir bulunamadı. Lütfen geçerli bir şehir adı girin.';
+    }
+}
+weatherForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    getWeather();
+});
